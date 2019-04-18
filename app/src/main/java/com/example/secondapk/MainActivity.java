9b14fcc9aca88e18b33edccaa46144160d4f54f8
kwing <gtict@sina.com>
2019-04-18 17:15:14 +0800

    22
    22

--- app/src/main/java/com/example/secondapk/MainActivity.java
+++ app/src/main/java/com/example/secondapk/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
-        Toast.makeText( this,"ddd",Toast.LENGTH_SHORT );
+        Toast tt=   Toast.makeText( this,"ddd",Toast.LENGTH_SHORT );
+        tt.show();
     }
 }
